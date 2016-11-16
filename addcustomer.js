7da6f21b8d0e8336ffd39515fba2997b0352fae4
eswaribala <Parameswaribala@gmail.com>
2016-11-16 13:04:57 +0530

    Add files via upload

---
+++ addcustomer.js
@@ -0,0 +1,15 @@
+﻿var customermodule = angular.module('CustomerModule', ['ngMessages']);
+
+customermodule.controller('AddCustomerController', ['$scope', function ($scope) {
+
+    $scope.customer = {
+        Name: "",
+        DOB: new Date(1990, 5, 5),
+        Address: "",
+        Email: "",
+        PhoneNo: 0,
+        SecretKey:""
+     
+    }
+
+}]);
